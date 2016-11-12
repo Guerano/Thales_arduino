ed56a7f2930591515871302647373741d69f3a45
Robin Rebischung <robin.rebischung@epita.fr>
2016-11-12 22:32:07 +0100

    Keypad 4x4 now handled
    Signed-off-by: Robin Rebischung <robin.rebischung@epita.fr>

--- thales/thales.ino
+++ thales/thales.ino
@@ -1,3 +1,35 @@
+#include <Keypad.h>
+
+/*
+-----------------
+| 1 | 2 | 3 | F |
+-----------------
+| 4 | 5 | 6 | E |
+-----------------
+| 7 | 8 | 9 | D |
+-----------------
+| A | 0 | B | C |
+-----------------
+| | | | |  |  |  |
+| | | | |  |  |  |
+9 8 7 6 13 12 11 10
+*/
+
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char keys[ROWS][COLS] = {
+    {'1','2','3', 'F'},
+    {'4','5','6', 'E'},
+    {'7','8','9', 'D'},
+    {'A','0','B', 'C'}
+};
+
+byte colPins[COLS] = { 13, 12, 11, 10 };
+byte rowPins[ROWS] = { 9, 8, 7, 6 };
+
+Keypad kpd = Keypad( makeKeymap(keys), colPins, rowPins, ROWS, COLS );
+
 void setup()
 {
     Serial.begin(115200);
@@ -5,6 +37,7 @@ void setup()
 
 void loop()
 {
-
+    char key = kpd.getKey();
+    if(key)
+        Serial.print(key);
 }
-
